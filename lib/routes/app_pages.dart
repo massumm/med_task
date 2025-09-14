b115ce94c8981998dccb3f9524bb7c4f51f86121
Masum Al Reza <masum321987@gmail.com>
2025-09-14 23:33:07 +0600

    Add article list and details views with controller
    Implemented ArticleController for fetching articles from API with pagination. Added Article model, list and details views, and updated routing to support article navigation. Changed initial route to HOME and integrated GetX for state management.

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,6 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
+import '../views/article_details_view.dart';
 import '../views/article_list_view.dart';
 import '../views/login_view.dart';
 import 'app_routes.dart';
@@ -8,5 +9,6 @@ class AppPages{
   static final pages = [
     GetPage(name: AppRoutes.LOGIN, page: () => LoginScreen()),
     GetPage(name: AppRoutes.HOME, page: () => ArticleListView()),
+    GetPage(name: AppRoutes.DETAIL, page: () => ArticleDetailsView()),
     ];
 }
--- lib/views/article_list_view.dart
+++ lib/views/article_list_view.dart
@@ -1,16 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:med_task/routes/app_routes.dart';
+import '../controllers/article_controller.dart';
 
-import 'package:flutter/cupertino.dart';
+class ArticleListView extends StatelessWidget {
+  final ArticleController controller = Get.put(ArticleController());
 
-class ArticleListView extends StatefulWidget {
-  const ArticleListView({super.key});
-
-  @override
-  State<ArticleListView> createState() => _ArticleListViewState();
-}
-
-class _ArticleListViewState extends State<ArticleListView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final scrollController = ScrollController();
+
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        controller.fetchArticles();
+      }
+    });
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Articles")),
+      body: RefreshIndicator(
+        onRefresh: () => controller.fetchArticles(refresh: true),
+        child: Obx(() {
+          if (controller.articles.isEmpty && controller.isLoading.value) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            controller: scrollController,
+            itemCount: controller.articles.length +
+                (controller.hasMore.value ? 1 : 0),
+            itemBuilder: (context, index) {
+              if (index < controller.articles.length) {
+                final article = controller.articles[index];
+                return ListTile(
+                  title: Text(article.title),
+                  subtitle: Text(article.body, maxLines: 2, overflow: TextOverflow.ellipsis),
+                  onTap: () => Get.toNamed(AppRoutes.DETAIL, arguments: article),
+                );
+              } else {
+                // loader at bottom
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              }
+            },
+          );
+        }),
+      ),
+    );
   }
 }
